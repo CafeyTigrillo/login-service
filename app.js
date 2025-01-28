3d27f8b0d0010fdfd26c864ac6fecf8ae16c73df
Edgar <epgarzonr@uce.edu.ec>
2025-01-27 23:00:14 -0500

    feat: add routes folder and app.js to initialize the server and define API endpoints

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(bodyParser.json());
+app.use('/api', loginRoutes);
+
+app.listen(PORT, async () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection successful.');
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+  }
+});
